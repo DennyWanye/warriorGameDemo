3097f9d6e1ac1a094fc1759905ed31d0fec65aa9
DennyWanye <2804989238@qq.com>
2025-12-04 16:37:32 +0800

    feat: finished course 244 Custom Latent Action

--- Source/Warrior/Public/WarriorTypes/WarriorEnumTypes.h
+++ Source/Warrior/Public/WarriorTypes/WarriorEnumTypes.h
@@ -21,3 +21,17 @@ enum class EWarriorSuccessType : uint8
 	Failed
 };
 
+UENUM()
+enum class EWarriorCountDownActionInput : uint8
+{
+	Start,
+	Cancel
+};
+
+UENUM()
+enum class EWarriorCountDownActionOutput : uint8
+{
+	Updated,
+	Completed,
+	Cancelled
+};
